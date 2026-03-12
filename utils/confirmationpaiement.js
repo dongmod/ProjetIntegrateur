11f821dbd9196083686eeccbcfdef45a8df8e169
VanezaGaytan <vaneza.cgaytan@email.com>
2026-03-12 10:50:31 -0400

    merge: feature/backend into feature/frontend-admin

--- utils/confirmationpaiement.js
+++ utils/confirmationpaiement.js
@@ -1,7 +1,18 @@
-import nodemailer from "nodemailer";
-<<<<<<< HEAD
-import {sendEmail} from "./email.js"; 
+// import nodemailer from "nodemailer";
 
+// import {sendEmail} from "./email.js"; 
+
+// // const transporter = nodemailer.createTransport({
+// //   service: "gmail",
+// //   auth: {
+// //     user: process.env.EMAIL_USER,
+// //     pass: process.env.EMAIL_PASS
+// //   }
+// // });
+
+// export async function confirmationpaiement(email) {
+//   return await sendEmail({ 
+// import { genererRecuPDF } from "./recu_paiement.js";
 // const transporter = nodemailer.createTransport({
 //   service: "gmail",
 //   auth: {
@@ -10,9 +21,30 @@ import {sendEmail} from "./email.js";
 //   }
 // });
 
-export async function confirmationpaiement(email) {
-  return await sendEmail({ 
-=======
+
+
+// export async function confirmationpaiement(email, factureid) {
+//   const pdfBuffer = await genererRecuPDF(factureid);
+//   await transporter.sendMail({
+
+//     from: `"Smart Garage" <${process.env.EMAIL_USER}>`,
+//     to: email,
+//     subject: "confirmation de paiement",
+//     html: `
+//       <h2>Votre paiement a été confirmé</h2>
+//       <p>merci de votre confiance.</p>
+//     `,
+//     attachments: [
+//       {
+//         filename: "votre facture.pdf",
+//         content: pdfBuffer
+//       }
+//     ]
+//   });
+
+
+
+import nodemailer from "nodemailer";
 import { genererRecuPDF } from "./recu_paiement.js";
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -21,22 +53,17 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS
   }
 });
-
-
-
+ 
+ 
+ 
 export async function confirmationpaiement(email, factureid) {
   const pdfBuffer = await genererRecuPDF(factureid);
   await transporter.sendMail({
->>>>>>> origin/feature/backend
     from: `"Smart Garage" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "confirmation de paiement",
     html: `
       <h2>Votre paiement a été confirmé</h2>
-<<<<<<< HEAD
-      <p>Merci de votre confiance.</p>
-    `
-=======
       <p>merci de votre confiance.</p>
     `,
      attachments: [
@@ -45,6 +72,7 @@ export async function confirmationpaiement(email, factureid) {
         content: pdfBuffer
       }
     ]
->>>>>>> origin/feature/backend
   });
 }
+ 
+ 
