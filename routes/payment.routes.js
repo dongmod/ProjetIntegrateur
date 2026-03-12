62f7c756fe7e2d536b59335f344567cc2df0bc52
stephanievasquez1516 <stephanie.vasquez1516@gmail.com>
2026-03-12 00:52:45 -0400

    Correction URLs Stripe - success et cancel vers port 3001

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -50,8 +50,8 @@ router.post("/creatiopayment", verifyToken, async (req, res) => {
 
 
       
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel",
+      success_url: "http://localhost:3001/success",
+      cancel_url: "http://localhost:3001/cancel",
       //retrouver  rendesvous_id dans factures
 
       metadata: {
